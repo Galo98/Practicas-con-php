4054f67df97ae9b7e71eb7f37fbe5fe1a5777cf1
Galo98 <galonahuel98@gmail.com>
2022-08-20 01:12:42 -0300

    identificacion de estaciones, no por dias, solo mes

--- punto2.php
+++ punto2.php
@@ -15,8 +15,24 @@
     <?php }else{
 
         $fechaIngresada = $_POST['fecha'];
+        
+        $fdia = $fechaIngresada[8] .$fechaIngresada[9];
 
-        echo "se ingreso la siguiente fecha " .$fechaIngresada;
+        $fmes = $fechaIngresada[5] .$fechaIngresada[6];
+
+        $faño = $fechaIngresada[0] .$fechaIngresada[1] .$fechaIngresada[2] .$fechaIngresada[3]; 
+
+        if($fmes >= 3 && $fmes <=6){
+           echo " <h1> Estación de Otoño</h1> ";
+        }else if($fmes >= 6 && $fmes <= 9){
+            echo " <h1> Estación de Invierno</h1> ";
+        }else if($fmes >= 9 && $fmes <= 12){
+            echo " <h1> Estación de Primavera</h1> ";
+        }else if($fmes == 12 || $fmes <= 3){
+            echo " <h1> Estación de Invierno</h1> ";
+        }
+
+        echo "<h3> Fecha Ingresada: " .$fdia ." / " .$fmes ." / " .$faño ." </h3>";
 
         ?>
         <form action="index.php" method="POST">
@@ -25,6 +41,18 @@
             <button>Recargar</button>
         </form>
 
-<?php } ?>
+<?php } 
+
+/* 
+Verano (21 de diciembre a 20 de marzo).
+Otoño (21 de marzo a 20 de junio).
+Invierno (21 de junio a 20 de septiembre).
+Primavera (21 de septiembre a 20 de diciembre). 
+*/
+
+
+?>
 
 </article>
+
+
