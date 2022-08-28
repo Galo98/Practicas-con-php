7ad25f6378c89b778169d1c07035ec7295901674
Galo98 <galonahuel98@gmail.com>
2022-08-28 14:24:48 -0300

    Configurando la maqueta punto3

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<html lang="es_ES">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title> Guia N°2 PHP </title>
     <?php include "partes/head.php" ?>
 </head>
@@ -11,13 +9,14 @@
 
     <?php include "partes/header.php" ?>
 
-<main>
+<main class="contenedor_ejercicios" >
+    
     <section class="punto2">
         <?php include "punto2.php" ?>
     </section>
 
     <section class="punto3" >
-        
+        <?php include "punto3.php" ?>
     </section>
 
 </main>
---
+++ punto3.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+        ----**** CONSIGNA ****-----
+
+    Crear un programa que me permita
+    - Ingresar día, mes y año.
+    - Validar si la fecha ingresada es correcta, sino que me pida reingresar (ej; 21/2/2022)
+    - Calcule cuantos dias faltan para llegar a la fecha actual
+    - Cuantos dias pasandoron desde la fecha actual
+    - Mostrarlo debajo del formulario de ingreso
+ */
+
+
+?>
+
+<article>
+    <h1>seccion 2</h1>
+</article>
