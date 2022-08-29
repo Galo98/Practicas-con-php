f88e55e2058250815e173d7836f48cc14807ac63
Galo98 <galonahuel98@gmail.com>
2022-08-29 17:19:18 -0300

    Verificaciones a las respuestas de fechas

--- punto3.php
+++ punto3.php
@@ -2,28 +2,60 @@
 <?php 
 $mostrar = 0;
 
-if(!isset($_POST['dia'])){
-$_POST['dia'] = 0;
-$_POST['mes'] = 0;
-$_POST['anio'] = 0;
-$mostrar = 1;
+    if(!isset($_POST['dia'])){
+        $_POST['dia'] = 0;
+        $_POST['mes'] = 0;
+        $_POST['anio'] = 0;
+        $mostrar = 1;
 
-}else if($_POST['dia'] <= 0 || $_POST['dia'] >= 31 || $_POST['mes'] <= 0 || $_POST['mes'] > 12 || $_POST['anio'] < 1){
-    $mostrar = 2;
-} else{
-    
+    }else if($_POST['dia'] <= 0 || $_POST['dia'] >= 31 || $_POST['mes'] <= 0 || $_POST['mes'] > 12 || $_POST['anio'] < 1){
+        $mostrar = 2;
+        } else{
+            
 
-    $fechaActual = date("Y-m-d");
-    
-    $fechaIngresada = $_POST['anio'] ."-" .$_POST['mes'] ."-" .$_POST['dia'];
-    
-    $mostrar = 3;
+            $fechaActual = date("Y-m-d");
+            
+            $fechaIngresada = $_POST['anio'] ."-" .$_POST['mes'] ."-" .$_POST['dia'];
 
-    $datos = diferenciasFechas($fechaIngresada, $fechaActual);
 
-    // revision de fechas
+            $datos = diferenciasFechas($fechaIngresada, $fechaActual);
 
-}
+            #region revision de fechas
+            $fActual = date("Y") .date("m") .date("d");
+
+            if($_POST['mes'] < 10){
+                $m = 0 .$_POST['mes'];
+            }else{
+                $m = $_POST['mes'];
+            }
+
+            if($_POST['dia'] < 10){
+                $d = 0 .$_POST['dia'];
+            }else{
+                $d = $_POST['dia'];
+            }
+
+            $fIngreso = $_POST['anio'] .$m .$d;
+            if($fActual > $fIngreso){
+                $mostrar = 3;
+            }else if($fActual < $fIngreso){
+                $mostrar = 4;
+            } else{
+                $mostrar = 5;
+            }
+
+            #endregion
+
+            #region fechas para mostrar
+            $diaIM = $_POST['dia'];
+            $mesIM = $_POST['mes'];
+            $añoIM = $_POST['anio'];
+
+            $diaAM = date("d");
+            $mesAM = date("m");
+            $añoAM = date("Y");
+            #endregion
+        }
 
 
 
@@ -41,7 +73,18 @@ function diferenciasFechas($fechaInicio, $fechaFin){
 }
 #endregion
 
+#region verificar mayores a 0
+function verificar($valor, $valor1){
 
+    $respuesta = " ";
+
+    if($valor >= 1){
+        $respuesta = $valor .$valor1;
+    }
+
+    return $respuesta;
+}
+#endregion
 ?>
 
 
@@ -63,21 +106,24 @@ function diferenciasFechas($fechaInicio, $fechaFin){
 
                 switch($mostrar){
                     case 1:
-                        echo "<h2 class='fechas__mensajes-titulo'>No se ingreso ninguna fecha</h2>";
+                        echo "<h2 class='fechas__mensajes-titulo'>No se ingreso ninguna fecha.</h2>";
                         break;
                     case 2:
-                        echo "<h2 class='fechas__mensajes-titulo'>Hay un error en la fecha, por favor reingrese los valores</h2>";
+                        echo "<h2 class='fechas__mensajes-titulo'>Hay un error en la fecha, por favor reingrese los valores.</h2>";
                         break;
                     case 3:
-                        echo "<h2 class='fechas__mensajes-titulo'>Resultados</h2>";
-                        echo "<p class='fechas__mensajes-texto'>Fecha actual " . $fechaActual ."</p>";
-                        echo "<p class='fechas__mensajes-texto'>Fecha ingresada " .$fechaIngresada ."</p>";
-                        echo "<p class='fechas__mensajes-texto'>Diferencia en años = " .$datos[0] ."</p>";
-                        echo "<p class='fechas__mensajes-texto'>Diferencia en meses = " .$datos[1] ."</p>";
-                        echo "<p class='fechas__mensajes-texto'>Diferencia en días = " .$datos[11] ."</p>";
+                        echo "<h2 class='fechas__mensajes-titulo'>Resultado</h2>";
+                        echo "<p class='fechas__mensajes-texto'>Pasaron al rededor de  " .verificar($datos[0], " años, ") .verificar($datos[1], " meses o ") .$datos[11] ." días, desde el día " .$diaIM ." del mes " .$mesIM ." del año " .$añoIM .".</p>";
                         break;
-
-                }
+                    case 4:
+                        echo "<h2 class='fechas__mensajes-titulo'>Resultado</h2>";
+                        echo "<p class='fechas__mensajes-texto'>Todavía falta  " .verificar($datos[0], " años, ") .$datos[1] ." meses o " .$datos[11] ." días, para llegar al día " .$diaIM ." del mes " .$mesIM ." del año " .$añoIM .".</p>";
+                        break;
+                    case 5;
+                        echo "<h2 class='fechas__mensajes-titulo'>Resultado</h2>";
+                        echo "<p class='fechas__mensajes-texto'>La fecha ingresada es la misma que la fecha actual.</p>";
+                        break;
+                    }
             ?>
         </div>
     </div>
