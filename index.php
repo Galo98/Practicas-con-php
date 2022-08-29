950ed67b72cc7db4fbe5d4cb8b2edec8fe0d6881
Galo98 <galonahuel98@gmail.com>
2022-08-29 15:50:37 -0300

    agregando estilos al punto 3

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    
     <title> Guia N°2 PHP </title>
     <?php include "partes/head.php" ?>
 </head>
 <body>
 
-    <?php include "partes/header.php" ?>
-
+        <?php include "partes/header.php" ?>
+    
 <main class="contenedor_ejercicios" >
     
     <section class="punto2">
--- punto3.php
+++ punto3.php
@@ -1,19 +1,23 @@
 
 <?php 
+$mostrar = 0;
 
 if(!isset($_POST['dia'])){
 $_POST['dia'] = 0;
 $_POST['mes'] = 0;
 $_POST['anio'] = 0;
-$mostrar = false;
-}else{
+$mostrar = 1;
+
+}else if($_POST['dia'] <= 0 || $_POST['dia'] >= 31 || $_POST['mes'] <= 0 || $_POST['mes'] > 12 || $_POST['anio'] < 1){
+    $mostrar = 2;
+} else{
     
 
     $fechaActual = date("Y-m-d");
     
     $fechaIngresada = $_POST['anio'] ."-" .$_POST['mes'] ."-" .$_POST['dia'];
     
-    $mostrar = true;
+    $mostrar = 3;
 
     $datos = diferenciasFechas($fechaIngresada, $fechaActual);
 }
@@ -37,25 +41,39 @@ function diferenciasFechas($fechaInicio, $fechaFin){
 
 
 <article class="contenedor__fechas">
-    <h1>Cuanto tiempo falta o cuanto tiempo paso</h1>
-    <form action="index.php" method="POST">
-        <label>Ingrese una fecha
-            <input type="number" name="dia" placeholder="día" min=1 max= 31 >
-            <input type="number" name="mes" placeholder="mes" min= 1 max= 12 >
-            <input type="number" name="anio" placeholder="año" >
-        </label>
-        <button> Enviar </button>
-    </form>
-    <div class="mensajes">
-        <?php
-            if($mostrar){
-                echo "Fecha actual" . $fechaActual;
-                echo "Fecha ingresada" .$fechaIngresada;
+    <div class = "transparencia">
+        <h1 class="fechas-titulo">Cuanto tiempo falta o cuanto tiempo paso</h1>
+        <form class="fechas-formulario" action="index.php" method="POST">
+            <label class="fechas__label">Ingrese una fecha
+                <div class="fechas__contenedor_campos">
+                    <input class="fechas__campo" type="number" name="dia" placeholder="día" min=1 max= 31 >
+                    <input class="fechas__campo" type="number" name="mes" placeholder="mes" min= 1 max= 12 >
+                    <input class="fechas__campo" type="number" name="anio" placeholder="año" >
+                </div>
+            </label>
+            <button class="fechas__btn"> Enviar </button>
+        </form>
+        <div class="fechas__mensajes">
+            <?php
 
-                echo "Años que pasaron = " .$datos[0];
-                echo "Meses que pasaron = " .$datos[1];
-                echo "Total Dias = " .$datos[11];
-            }
-        ?>
+                switch($mostrar){
+                    case 1:
+                        echo "<h2 class='fechas__mensajes-titulo'>No se ingreso ninguna fecha</h2>";
+                        break;
+                    case 2:
+                        echo "<h2 class='fechas__mensajes-titulo'>Hay un error en la fecha, por favor reingrese los valores</h2>";
+                        break;
+                    case 3:
+                        echo "<h2 class='fechas__mensajes-titulo'>Resultados</h2>";
+                        echo "<p class='fechas__mensajes-texto'>Fecha actual " . $fechaActual ."</p>";
+                        echo "<p class='fechas__mensajes-texto'>Fecha ingresada " .$fechaIngresada ."</p>";
+                        echo "<p class='fechas__mensajes-texto'>Diferencia en años = " .$datos[0] ."</p>";
+                        echo "<p class='fechas__mensajes-texto'>Diferencia en meses = " .$datos[1] ."</p>";
+                        echo "<p class='fechas__mensajes-texto'>Diferencia en días = " .$datos[11] ."</p>";
+                        break;
+
+                }
+            ?>
+        </div>
     </div>
 </article>
