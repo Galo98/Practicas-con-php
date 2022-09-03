9c5914cbeb69497496676b38cdd487aa422e838f
Galo98 <galonahuel98@gmail.com>
2022-09-03 19:17:33 -0300

    Seccion busqueda completa

--- vistaAdmin/busqueda.php
+++ vistaAdmin/busqueda.php
@@ -1,14 +1,42 @@
 <?php
-
-    require "../conexion.php";
-
     session_start();
+
     $sid = $_SESSION['id'];
     $snom = $_SESSION['nombre'];
     $srol = $_SESSION['rol'];
 
-    $query = "select * from producto;";
-    $consulta = mysqli_query(conectar(),$query);
+    require "../conexion.php";
+    $conn = conectar();
+
+    $mensaje = 0;
+
+    if(isset($_POST['valor'])){
+        $buscar = $_POST['valor'];
+        $opcion = $_POST['opcion'];
+
+        switch($opcion){
+            case 1: 
+                $query = "select * from producto where pid = $buscar;";
+                $consulta = mysqli_query($conn,$query);
+                break;
+            case 2:
+                $query = "select * from producto where pdesc = '$buscar';";
+                $consulta = mysqli_query($conn,$query);
+                break;
+            case 3:
+                $query = "select * from producto where pprecio = $buscar;";
+                $consulta = mysqli_query($conn,$query);
+                break;
+        }
+        if(mysqli_affected_rows($conn) > 0){
+            $mensaje = 1;
+        }else{
+            $mensaje = 2;
+        }
+    }else{
+        $query = "select * from producto;";
+        $consulta = mysqli_query($conn,$query);
+    }
 
 ?>
 <!DOCTYPE html>
@@ -24,6 +52,17 @@
 <body>
     <header class="cabecera">
             <h2 class="cabecera-titulo"><a class="titulo-enlace" href="index.php">Guia N°2 PHP</a></h2>
+            <div>
+                <form action="busqueda.php" method="POST">
+                    <input type="text" name="valor" id="buscador" required><label for="buscador" class="oculto">Ingresar Texto para Buscar</label>
+                    <button>Buscar</button>
+                    <div>
+                        <input type="radio" name="opcion" value=1 id="id" required><label for="id">Buscar por ID</label>
+                        <input type="radio" name="opcion" value=2 id="nombre" required><label for="nombre">Buscar por Nombre</label>
+                        <input type="radio" name="opcion" value=3 id="precio" required><label for="precio">Buscar or Precio</label>
+                    </div>
+                </form>
+            </div>
             <nav class="cabecera__nav">
                 <ul>
                     <li><a href="admin.php">Home</a></li>
@@ -36,6 +75,17 @@
     <main>
 
         <section class="productos">
+            <?php switch($mensaje){
+                case 0:?>
+                    <h1>Lista de todos los productos actuales</h1>
+                <?php break;
+                case 1:?>
+                    <h1>Productos encontrados tras buscar <?php echo $buscar; ?></h1>
+                <?php break;
+                case 3:?>
+                    <h1>No se encontraron resultados en la busqueda de <?php echo $buscar; ?></h1>
+                <?php break;
+                } ?>
             <?php while($dato = mysqli_fetch_assoc($consulta)){ ?>
                 <article>
                     <h2><?php echo $dato['pdesc']; ?></h2>
