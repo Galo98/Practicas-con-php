4f66d43a8a4e4093ac653a59f26a7632c3550656
Galo98 <galonahuel98@gmail.com>
2022-09-04 16:29:09 -0300

    agregando menu

--- cerrarSesion.php
+++ cerrarSesion.php
@@ -18,8 +18,11 @@
 <body>
 <header class="cabecera">
         <h2 class="cabecera-titulo"><a class="titulo-enlace" href="index.php">Guia N°2 PHP</a></h2>
-        <nav class="cabecera__nav"></nav>
-        <span class="cabecera__span"><a class="titulo-enlace" href="login.php">Iniciar Session</a></span>
+        <nav class="cabecera__nav">
+            <a class="titulo-enlace" href="index.php">Home</a>
+            <a class="titulo-enlace" href="login.php">Iniciar Session</a>
+        </nav>
+        <span class="cabecera__span"></span>
 </header>
 
     <section>
